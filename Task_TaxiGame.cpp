aea0f073cd45133f1e878af5247be011363666c6
宮川 龍希 <23ci0538@jec.ac.jp>
2024-06-27 09:55:12 +0900

    タクシーゲームタスクを追加

---
+++ Task_TaxiGame.cpp
@@ -0,0 +1,139 @@
+//-------------------------------------------------------------------
+//ゲーム本編
+//-------------------------------------------------------------------
+#include  "MyPG.h"
+#include  "Task_Game.h"
+
+#include "randomLib.h"
+
+#include  "Task_Ending.h"
+
+namespace  Game
+{
+	Resource::WP  Resource::instance;
+	//-------------------------------------------------------------------
+	//リソースの初期化
+	bool  Resource::Initialize()
+	{
+		return true;
+	}
+	//-------------------------------------------------------------------
+	//リソースの解放
+	bool  Resource::Finalize()
+	{
+		return true;
+	}
+	//-------------------------------------------------------------------
+	//「初期化」タスク生成時に１回だけ行う処理
+	bool  Object::Initialize()
+	{
+		//スーパークラス初期化
+		__super::Initialize(defGroupName, defName, true);
+		//リソースクラス生成orリソース共有
+		res = Resource::Create();
+
+		//★データ初期化
+		
+		//デバッグ用フォントの準備
+		TestFont = DG::Font::Create("ＭＳ ゴシック", 30, 30);
+
+		//★タスクの生成
+
+		return  true;
+	}
+	//-------------------------------------------------------------------
+	//「終了」タスク消滅時に１回だけ行う処理
+	bool  Object::Finalize()
+	{
+		//★データ＆タスク解放
+		ge->KillAll_G("本編");
+
+
+		if (!ge->QuitFlag() && nextTaskCreate) {
+			//★引き継ぎタスクの生成
+			auto next = Ending::Object::Create(true);
+		}
+
+		return  true;
+	}
+	//-------------------------------------------------------------------
+	//「更新」１フレーム毎に行う処理
+	void  Object::UpDate()
+	{
+		auto inp = ge->in1->GetState( );
+		if (inp.ST.down) {
+			ge->StartCounter("test", 45); //フェードは90フレームなので半分の45で切り替え
+			ge->CreateEffect(99, ML::Vec2(0, 0));
+		}
+		if (ge->getCounterFlag("test") == ge->LIMIT) {
+			Kill();
+		}
+	}
+	//-------------------------------------------------------------------
+	//「２Ｄ描画」１フレーム毎に行う処理
+	void  Object::Render2D_AF()
+	{
+		int x = GetRandom(-10, 10);
+		int y = GetRandom(-10, 10);
+
+		TestFont->Draw(ML::Box2D(100+x, 100+y, ge->screen2DWidth, ge->screen2DHeight),
+			"Game"
+		);
+
+	}
+
+	//★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+	//以下は基本的に変更不要なメソッド
+	//★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+	//-------------------------------------------------------------------
+	//タスク生成窓口
+	Object::SP  Object::Create(bool  flagGameEnginePushBack_)
+	{
+		Object::SP  ob = Object::SP(new  Object());
+		if (ob) {
+			ob->me = ob;
+			if (flagGameEnginePushBack_) {
+				ge->PushBack(ob);//ゲームエンジンに登録
+			}
+			if (!ob->B_Initialize()) {
+				ob->Kill();//イニシャライズに失敗したらKill
+			}
+			return  ob;
+		}
+		return nullptr;
+	}
+	//-------------------------------------------------------------------
+	bool  Object::B_Initialize()
+	{
+		return  Initialize();
+	}
+	//-------------------------------------------------------------------
+	Object::~Object() { B_Finalize(); }
+	bool  Object::B_Finalize()
+	{
+		auto  rtv = Finalize();
+		return  rtv;
+	}
+	//-------------------------------------------------------------------
+	Object::Object() {	}
+	//-------------------------------------------------------------------
+	//リソースクラスの生成
+	Resource::SP  Resource::Create()
+	{
+		if (auto sp = instance.lock()) {
+			return sp;
+		}
+		else {
+			sp = Resource::SP(new  Resource());
+			if (sp) {
+				sp->Initialize();
+				instance = sp;
+			}
+			return sp;
+		}
+	}
+	//-------------------------------------------------------------------
+	Resource::Resource() {}
+	//-------------------------------------------------------------------
+	Resource::~Resource() { Finalize(); }
+}
