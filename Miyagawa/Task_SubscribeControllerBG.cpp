d08c2c9a6a1eedd50b962cc83fe0bd0d63c91b19
宮川 龍希 <23ci0538@jec.ac.jp>
2024-10-16 16:39:24 +0900

    コントローラー登録画面のイージング追加(不安定)、背景追加 タクシーゲームの人数調整に対応

--- Miyagawa/Task_SubscribeControllerBG.cpp
+++ Miyagawa/Task_SubscribeControllerBG.cpp
@@ -1,26 +1,24 @@
 //-------------------------------------------------------------------
-//コントローラー登録画面
+//最終リザルト画面背景
 //-------------------------------------------------------------------
 #include  "../MyPG.h"
-#include  "Task_SubscribeController.h"
 #include  "Task_SubscribeControllerBG.h"
-#include  "../Task_Game.h"
-#include  "../sound.h"
-#include  "../easing.h"
 
-namespace SubscribeController
+namespace  SubscribeControllerBG
 {
 	Resource::WP  Resource::instance;
 	//-------------------------------------------------------------------
 	//リソースの初期化
 	bool  Resource::Initialize()
 	{
+		image = DG::Image::Create("./data/image/SubscribeBG.jpg");
 		return true;
 	}
 	//-------------------------------------------------------------------
 	//リソースの解放
 	bool  Resource::Finalize()
 	{
+		image.reset();
 		return true;
 	}
 	//-------------------------------------------------------------------
@@ -33,39 +31,22 @@ namespace SubscribeController
 		res = Resource::Create();
 
 		//★データ初期化
-		render2D_Priority[1] = 0.f;
-		TestFont = DG::Font::Create("ＭＳ ゴシック", 30, 30);
-		ge->players.clear();
-
-		//コンテナに追加
-		inputs.push_back(ge->in1);
-		inputs.push_back(ge->in2);
-		inputs.push_back(ge->in3);
-		inputs.push_back(ge->in4);
-
+		render2D_Priority[1] = 1.f;
+		moveVec.x = 3.f;
+		
 		//★タスクの生成
-		//背景
-		//横スクロールを作る
-		for (int i = 0; i < 3; ++i)
-		{
-			auto BG = SubscribeControllerBG::Object::Create(true);
-			if (BG)
-			{
-				BG->pos.x = static_cast<float>(i * 1600);
-			}
-		}
+
 		return  true;
 	}
 	//-------------------------------------------------------------------
 	//「終了」タスク消滅時に１回だけ行う処理
 	bool  Object::Finalize()
 	{
-
-		ge->KillAll_G("コントローラー登録画面");
 		//★データ＆タスク解放
+
+
 		if (!ge->QuitFlag() && nextTaskCreate) {
 			//★引き継ぎタスクの生成
-			Game::Object::Create(true);
 		}
 
 		return  true;
@@ -74,58 +55,29 @@ namespace SubscribeController
 	//「更新」１フレーム毎に行う処理
 	void  Object::UpDate()
 	{
-		easing::UpDate();//必須
-		for (int i = 0; i < inputs.size(); ++i) {
-			//次のタスクへ
-			if (inputs[i]->GetState().ST.down && subscribeCnt > 0) {
-				Kill();
-			}
+		//背景の移動
+		pos -= moveVec;
 
-			//登録 
-			if (inputs[i]->GetState().B1.down) {
-				easing::Set("Reaction" + to_string(controllerIndex[i]), easing::QUADOUT, 0, 50, 5, "ReactionEnd" + to_string(controllerIndex[i]));
-				easing::Set("ReactionEnd" + to_string(controllerIndex[i]), easing::QUADOUT, 50, 0, 5);
-				easing::Start("Reaction" + to_string(controllerIndex[i]));
-				if (!isPushButton[i]) {
-					Subscribe(inputs[i], isPushButton[i], i);
-				}
-			}
+		//移動した距離を増やす
+		movedDistance += moveVec.x;
+
+		//背景が移動しきったら、最初の位置に戻す
+		if (movedDistance > 1600)
+		{
+			pos.x += movedDistance;
+			movedDistance = 0.f;
 		}
-
-		for (int i = 0; i < ge->players.size(); ++i) {
-			if (easing::GetState("ReactionEnd" + to_string(controllerIndex[i])) == easing::EQ_STATE::EQ_END) {
-				easingPos[controllerIndex[i]] = 0.f;
-			}
-			else if (easing::GetState("Reaction" + to_string(controllerIndex[i])) == easing::EQ_STATE::EQ_END) {
-				easingPos[controllerIndex[i]] = easing::GetPos("ReactionEnd" + to_string(controllerIndex[i]));
-			}
-			else
-			{
-				easingPos[controllerIndex[i]] = easing::GetPos("Reaction" + to_string(controllerIndex[i]));
-			}
-		}
-
 	}
 	//-------------------------------------------------------------------
 	//「２Ｄ描画」１フレーム毎に行う処理
 	void  Object::Render2D_AF()
 	{
-		for (int i = 0; i < ge->players.size(); ++i) {
-			if (!isPushButton[controllerIndex[i]]) { continue; }
-			ML::Box2D textBox(100 * i, 100 + easingPos[i], 100, 100);
-			string text = to_string(i + 1) + "P";
-			TestFont->Draw(textBox, text);
-		}
+		ML::Box2D src(0, 0, 1600, 1200);
+		ML::Box2D draw(0, 0, src.w, src.h);
+		draw.Offset(pos);
+		res->image->Draw(draw, src);
 	}
-	//-------------------------------------------------------------------
-	//「２Ｄ描画」１フレーム毎に行う処理
-	void  Object::Subscribe(XI::GamePad::SP controller, bool& isPush, const int& i)
-	{
-		isPush = true;
-		ge->players.push_back(controller);
-		controllerIndex[subscribeCnt] = i;
-		++subscribeCnt;
-	}
+
 	//★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
 	//以下は基本的に変更不要なメソッド
 	//★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
@@ -138,7 +90,7 @@ namespace SubscribeController
 			ob->me = ob;
 			if (flagGameEnginePushBack_) {
 				ge->PushBack(ob);//ゲームエンジンに登録
-
+				
 			}
 			if (!ob->B_Initialize()) {
 				ob->Kill();//イニシャライズに失敗したらKill
@@ -160,7 +112,10 @@ namespace SubscribeController
 		return  rtv;
 	}
 	//-------------------------------------------------------------------
-	Object::Object() {}
+	Object::Object()
+		:
+		moveVec(0.f, 0.f), movedDistance(0.f), pos(0.f, 0.f)
+	{	}
 	//-------------------------------------------------------------------
 	//リソースクラスの生成
 	Resource::SP  Resource::Create()
